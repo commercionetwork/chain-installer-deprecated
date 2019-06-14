265bf6ca43586b0549f772906d43b87e54a7dff2
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-06-14 09:40:41 +0200

    Added some missing documentation

--- utils/utils.go
+++ utils/utils.go
@@ -1,22 +1,28 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"strings"
 )
 
+// CheckError correctly displays the given error to the user, and exits the program with non-zero code
 func CheckError(err error) {
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(0)
 	}
 }
 
+// GetUserHome allow to return the string representing the path to the user home directory
 func GetUserHome() string {
 	home, err := os.UserHomeDir()
 	CheckError(err)
 	return home
 }
 
+// ReplaceLast returns the `original` string having the last occurrence of the specified `old` string replaced with the
+// specified `replace` string
 func ReplaceLast(original, old, replace string) string {
 	i := strings.LastIndex(original, old)
 
